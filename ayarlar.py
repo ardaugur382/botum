3800f1d74ef73bb6855ab2eaa4ca7a7a2bea9c12
ardaugur382 <144523775+ardaugur382@users.noreply.github.com>
2023-10-21 09:59:51 +0300

    Update ayarlar.py

--- ayarlar.py
+++ ayarlar.py
@@ -2,5 +2,5 @@
 
 ayarlar = {
     "on_taki": ">", #ön takı, prefix (maybe it won't work if we call it different here)
-    "TOKEN": "MTE1NTA2MTIyOTA2NTM0NzEwMg.GQN7SJ.qyplgARfYMaU84mbZPPTQysrBHYjGKyIYrZjd0"
+    "TOKEN": ""
 }
